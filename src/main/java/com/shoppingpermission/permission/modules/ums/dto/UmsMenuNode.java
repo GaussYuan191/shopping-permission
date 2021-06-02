e18bb610fea49e4c7b8e834babc0770c1556f6f9
GaussYuan191 <1915059302@qq.com>
2021-06-02 13:20:54 +0800

    后台节点封装

---
+++ src/main/java/com/shoppingpermission/permission/modules/ums/dto/UmsMenuNode.java
@@ -0,0 +1,10 @@
+package com.shoppingpermission.permission.modules.ums.dto;
+
+/**
+ * @program: shopping-permission
+ * @description: 后台节点封装
+ * @author: Gauss
+ * @date: 2021-06-02 11:26
+ **/
+public class UmsMenuNode {
+}

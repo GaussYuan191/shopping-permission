ea912d44f663c944c4442c954e6645be1233deae
GaussYuan191 <1915059302@qq.com>
2021-06-02 13:19:43 +0800

    后台用户和角色关系表 Mapper 接口

---
+++ src/main/java/com/shoppingpermission/permission/modules/ums/mapper/UmsAdminRoleRelationMapper.java
@@ -0,0 +1,10 @@
+package com.shoppingpermission.permission.modules.ums.mapper;
+
+/**
+ * @program: shopping-permission
+ * @description: 后台用户和角色关系表 Mapper 接口
+ * @author: Gauss
+ * @date: 2021-06-02 13:13
+ **/
+public interface UmsAdminRoleRelationMapper {
+}

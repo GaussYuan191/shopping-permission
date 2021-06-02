f375e0822b5987a3c3992828418a990fa9a8f477
GaussYuan191 <1915059302@qq.com>
2021-06-02 13:20:09 +0800

    管理员角色关系管理Service实现类

---
+++ src/main/java/com/shoppingpermission/permission/modules/ums/service/impl/UmsAdminRoleRelationServiceImpl.java
@@ -0,0 +1,17 @@
+package com.shoppingpermission.permission.modules.ums.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.shoppingpermission.permission.modules.ums.mapper.UmsAdminRoleRelationMapper;
+import com.shoppingpermission.permission.modules.ums.model.UmsAdminRoleRelation;
+import com.shoppingpermission.permission.modules.ums.service.UmsAdminRoleRelationService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: shopping-permission
+ * @description: 管理员角色关系管理Service实现类
+ * @author: Gauss
+ * @date: 2021-06-02 13:12
+ **/
+@Service
+public class UmsAdminRoleRelationServiceImpl extends ServiceImpl<UmsAdminRoleRelationMapper, UmsAdminRoleRelation> implements UmsAdminRoleRelationService {
+}

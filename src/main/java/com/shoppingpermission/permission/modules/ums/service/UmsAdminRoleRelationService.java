abccd4204e8d732b98f88bb53b05d6de29b759f4
GaussYuan191 <1915059302@qq.com>
2021-06-02 13:19:56 +0800

    管理员角色关系管理Service

---
+++ src/main/java/com/shoppingpermission/permission/modules/ums/service/UmsAdminRoleRelationService.java
@@ -0,0 +1,13 @@
+package com.shoppingpermission.permission.modules.ums.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shoppingpermission.permission.modules.ums.model.UmsAdminRoleRelation;
+
+/**
+ * @program: shopping-permission
+ * @description: 管理员角色关系管理Service
+ * @author: Gauss
+ * @date: 2021-06-02 13:14
+ **/
+public interface UmsAdminRoleRelationService extends IService<UmsAdminRoleRelation> {
+}

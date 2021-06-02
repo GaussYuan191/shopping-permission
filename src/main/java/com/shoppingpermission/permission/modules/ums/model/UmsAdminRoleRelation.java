a5e53df65bcc4a7747cf327a92e553c24a0d121a
GaussYuan191 <1915059302@qq.com>
2021-06-02 13:19:27 +0800

    后台用户和角色关系表

---
+++ src/main/java/com/shoppingpermission/permission/modules/ums/model/UmsAdminRoleRelation.java
@@ -0,0 +1,10 @@
+package com.shoppingpermission.permission.modules.ums.model;
+
+/**
+ * @program: shopping-permission
+ * @description: 后台用户和角色关系表
+ * @author: Gauss
+ * @date: 2021-06-02 13:15
+ **/
+public class UmsAdminRoleRelation {
+}

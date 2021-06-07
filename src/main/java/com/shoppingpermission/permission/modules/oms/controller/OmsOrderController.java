3ab6b8c931725bbddc89dcf946b43fd3dd0150eb
GaussYuan191 <1915059302@qq.com>
2021-06-07 13:43:26 +0800

    订单管理Controller

---
+++ src/main/java/com/shoppingpermission/permission/modules/oms/controller/OmsOrderController.java
@@ -0,0 +1,37 @@
+package com.shoppingpermission.permission.modules.oms.controller;
+
+import com.shoppingpermission.permission.common.api.CommonResult;
+import com.shoppingpermission.permission.modules.oms.model.OmsOrder;
+import com.shoppingpermission.permission.modules.oms.service.OmsOrderService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @program: shopping-permission
+ * @description: 订单管理Controller
+ * @author: Gauss
+ * @date: 2021-06-07 12:01
+ **/
+@Controller
+@RequestMapping("/order")
+public class OmsOrderController {
+
+    @Autowired
+    OmsOrderService orderService;
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public CommonResult list(@Param("id") Long id){
+        List<OmsOrder> orderList= orderService.detail(id);
+        System.out.println(orderList);
+        if (orderList == null) {
+            return CommonResult.failed();
+        }
+        return CommonResult.success(orderList);
+    }
+}

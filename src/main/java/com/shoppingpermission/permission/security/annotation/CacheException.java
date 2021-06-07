5d7609528be59be62c7891c701a8901b0df98d81
GaussYuan191 <1915059302@qq.com>
2021-06-07 13:42:37 +0800

    自定义注解，有该注解的缓存方法会抛出异常

---
+++ src/main/java/com/shoppingpermission/permission/security/annotation/CacheException.java
@@ -0,0 +1,10 @@
+package com.shoppingpermission.permission.security.annotation;
+
+/**
+ * @program: shopping-permission
+ * @description: 自定义注解，有该注解的缓存方法会抛出异常
+ * @author: Gauss
+ * @date: 2021-06-05 13:43
+ **/
+public interface CacheException {
+}
